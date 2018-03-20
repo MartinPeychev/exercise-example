222cfc0fbc70b6afff933ba46ffa23d06edb3e1a
MartinPeychev <Martinpismymail@gmail.com>
2018-03-20 22:40:11 +0200

    Add files via upload

---
+++ MagickLetters/MagickLetters/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MagickLetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char first = char.Parse(Console.ReadLine());
+            char second = char.Parse(Console.ReadLine());
+            char toAvoid = char.Parse(Console.ReadLine());
+
+            for (char i = first; i <= second; i++)
+            {
+                for (char j = first; j <= second; j++)
+                {
+                    for (char k = first; k <=second; k++)
+                    {
+                        if (i != toAvoid&&j!=toAvoid&&k!=toAvoid)
+                        {
+                            Console.Write($"{i}{j}{k} ");
+
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+}
